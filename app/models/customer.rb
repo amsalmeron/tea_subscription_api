f7d7d3f520633d74c31e5acdcd23813ce5bf213a
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-09-15 09:43:54 -0700

    Add: customer instance method that returns subscription ids

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,10 @@ class Customer < ApplicationRecord
     has_many :subscriptions, through: :customer_subscriptions
     has_many :subscription_teas, through: :subscriptions
     has_many :teas, through: :subscription_teas
+
+    def get_subscription_ids
+        subscriptions.map do |subscription|
+            subscription.id
+        end
+    end
 end
