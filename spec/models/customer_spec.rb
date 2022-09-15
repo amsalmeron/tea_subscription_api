7501ecd132010f626c15b9d273b2101edf1eff7b
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-09-15 09:43:08 -0700

    Test: customer instance method to return all subscription ids

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Customer, type: :model do
         it { should have_many(:subscription_teas).through(:subscriptions) } 
         it { should have_many(:teas).through(:subscription_teas) } 
     end
+
+    describe "instance methods" do
+        it 'returns ids of all subscriptions for a customer' do
+            customer = create(:customer)
+            subscriptions = create_list(:subscription, 3)
+            CustomerSubscription.create(customer_id: customer.id, subscription_id: subscriptions.first.id)
+            CustomerSubscription.create(customer_id: customer.id, subscription_id: subscriptions.third.id)
+            expect(customer.get_subscription_ids).to eq([subscriptions.first.id,subscriptions.third.id]) 
+        end
+    end
+    
 end
