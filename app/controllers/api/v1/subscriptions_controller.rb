2ad560063d344dc8b12301bfdbfbd0cbca38f123
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-09-15 10:53:29 -0700

    Add: update action to subscription controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -8,9 +8,18 @@ class Api::V1::SubscriptionsController < ApplicationController
         customer = Customer.find_by(id: params["customer_id"])
         subscription = Subscription.find_by(id: params["subscription_id"])
         if !customer.get_subscription_ids.include?(subscription.id)
-            CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription.id)
+            CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription.id, status: true)
         end
         subscription.update(status: true)
         render json: SubscriptionSerializer.new(subscription)
+    end
+    
+    def update
+        subscription = Subscription.find_by(id: params["subscription_id"])
+        customer_subscription = CustomerSubscription.find_by(customer_id: params["customer_id"], subscription_id: params["subscription_id"])
+        customer_subscription.toggle(:status)
+        customer_subscription.save
+
+        render json: { message: "#{subscription.title} has been cancelled"}
     end
 end
