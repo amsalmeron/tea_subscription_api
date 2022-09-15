3b3fe988f668815f656469e784ee5c373853f332
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-09-15 09:17:48 -0700

    Test: endpoint to receive all customer subscriptions

---
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Customer Subscription API' do
+    it "returns the list of past and present subscriptions for a customer" do
+        customer = create(:customer)
+        customer_2 = create(:customer)
+        subscriptions = create_list(:subscription, 3)
+        teas = create_list(:tea, 5)
+        CustomerSubscription.create(customer_id: customer.id, subscription_id: subscriptions.first.id)
+        CustomerSubscription.create(customer_id: customer.id, subscription_id: subscriptions.third.id)
+        CustomerSubscription.create(customer_id: customer_2.id, subscription_id: subscriptions.second.id)
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+        get '/api/v1/subscriptions', headers: headers, params: { customer_id: "#{customer.id}" }
+        
+        
+        expect(response).to be_successful
+        expect(response).to  have_http_status(200)
+        
+        response_body = JSON.parse(response.body, symbolize_names: true)
+        customer_subscriptions = response_body[:data]
+
+        expect(customer_subscriptions.count).to eq(2) 
+        customer_subscriptions.each do |subscription|
+            expect(subscription).to have_key(:id)
+            expect(subscription).to have_key(:type)
+            expect(subscription).to have_key(:attributes)
+            expect(subscription[:attributes][:title]).to be_a String
+            expect(subscription[:attributes][:status]).to be_in([true, false])
+            expect(subscription[:attributes][:price]).to be_a Integer
+            expect(subscription[:attributes][:frequency]).to be_a Integer
+        end
+    end
+end
