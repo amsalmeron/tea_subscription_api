426f48b78d45962e67081a64f60be5689eb786af
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-09-15 09:44:18 -0700

    Add: create action to subscription controller that add subscription to customers

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::SubscriptionsController < ApplicationController
         customer = Customer.find_by(id: params["customer_id"])
         render json: SubscriptionSerializer.new(customer.subscriptions)
     end
+    
+    def create
+        customer = Customer.find_by(id: params["customer_id"])
+        subscription = Subscription.find_by(id: params["subscription_id"])
+        if !customer.get_subscription_ids.include?(subscription.id)
+            CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription.id)
+        end
+        subscription.update(status: true)
+        render json: SubscriptionSerializer.new(subscription)
+    end
 end
